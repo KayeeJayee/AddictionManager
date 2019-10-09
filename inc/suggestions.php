2c61b484fe76224ad45b168a7e4f5c6851873026
Shakaoaa <39017160+Shakaoaa@users.noreply.github.com>
2019-10-09 16:39:41 +1300

    format

--- inc/suggestions.php
+++ inc/suggestions.php
@@ -1,11 +1,13 @@
 <?php
 include 'config.php';
+//Grabs a random number between 1 and 6, whichever number has been selected that suggestion 
+//in the suggestion table will be grabbed and placed on the site for the user
 $rand = rand(1,6);
 
 $query = "SELECT * FROM suggestions WHERE id = '$rand'";
 $result = mysqli_query($conn, $query);
 
 $row = mysqli_fetch_assoc($result);
-echo $row["sugg"]; // find a place to put this 
+echo $row["sugg"]; //testing
 
 ?>
