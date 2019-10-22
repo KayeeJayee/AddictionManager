44c7e9823f0283481083132a2ff2fbd4e1958b47
Shakaoaa <39017160+Shakaoaa@users.noreply.github.com>
2019-10-22 16:23:03 +1300

    need checking

--- inc/add_addiction.php
+++ inc/add_addiction.php
@@ -4,13 +4,14 @@ include 'config.php';
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 	$addiction = trim($_POST['addiction']);
 	$user_id = $_SESSION['user_id'];
+	$money = $_POST['money']
 	$date = date("Y-m-d");
 	// save data as new addiction in database
 	$sql = "INSERT INTO addiction(
 			user_id, name, date_created, 
-			money_goal, money_achieved, 
+			spending, money_achieved, 
 			streak_goal, streak_achieved) 
-	VALUES ('".$user_id."','".$addiction."','".$date."', 50, 0, 7, 1)";
+	VALUES ('".$user_id."','".$addiction."','".$date."', '".$money."', 0, 7, 1)"; //change money_goal column to spending
 	try{
 		mysqli_query($conn, $sql);
 		// return to home page
--- inc/check_in.php
+++ inc/check_in.php
@@ -8,7 +8,8 @@ if(isset($_GET['id']) && isset($_GET['check'])){
 	$addiction = mysqli_query($conn, "SELECT * FROM addiction WHERE id ='".$id."'");
 	$row = mysqli_fetch_assoc($addiction);
 	$achieved = $_GET['check'] == "y" ? true : false;
-	$streak = (int)$row['streak_achieved']; 
+	$streak = (int)$row['streak_achieved'];
+	$save = (int)$row['saving']; //added this
 	// check if achieved
 	if($achieved){
 		// check if checked-in on time
@@ -22,7 +23,9 @@ if(isset($_GET['id']) && isset($_GET['check'])){
 		else
 		{ // all good, update streak
 			$streak++;
+			$moneysaved = $row['money_achieved'] + $save; //Added this
 			$sql = "UPDATE addiction SET streak_achieved = '".$streak."' WHERE id ='".$id."'";
+			$sql2 = "UPDATE addiction SET money_achieved = '".$moneysaved."' WHERE id ='".$id."'"; //added this
 		}
 	// else user said no
 	}else{
@@ -31,6 +34,7 @@ if(isset($_GET['id']) && isset($_GET['check'])){
 	}
 	// send data and redirect
 	mysqli_query($conn, $sql);
+	mysqli_query($conn, $sql2); //added this
 	include 'functions.php';
 	updateStreakGoal($streak, $row['streak_goal'], $id);
 	header("Location: ../index.php");
